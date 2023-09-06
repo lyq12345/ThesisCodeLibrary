bcbcb2ca6aa34130b7830bd1402c1b19aa4c250b
lyq12345 <yuqiaol5@uci.edu>
2023-09-05 21:36:33 -0700

    firedetection tinyyolov3 fixed

--- firedetection/tinyyolov3/app.py
+++ firedetection/tinyyolov3/app.py
@@ -11,46 +11,14 @@ frame_data = None
 camera_index = 0
 
 def generate_frames():
-    # onboard compute
-    capture = cv2.VideoCapture(camera_index)
-    start_time = time.time()
     while True:
-        ret, frame = capture.read()
-        if not ret:
-            break
+        if frame_data is not None:
+            # 将帧数据编码为JPEG格式
+            _, jpeg = cv2.imencode('.jpg', frame_data)
 
-        # 计算帧率
-        current_time = time.time()
-        elapsed_time = current_time - start_time
-        fps = 1 / elapsed_time
-        start_time = current_time
-
-        # 在帧上绘制帧率
-        fps_text = "FPS: {:.2f}".format(fps)
-
-        # do onboard compute
-        processed_frame, detections = detect_fire_from_img(frame)
-
-        cv2.putText(processed_frame, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        # encode the captured frame
-        _, jpeg_frame = cv2.imencode('.jpg', processed_frame)
-
-
-        # 生成视频流
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + jpeg_frame.tobytes() + b'\r\n')
-
-    capture.release()
-    # detect from frames
-    # while True:
-    #     if frame_data is not None:
-    #         # 将帧数据编码为JPEG格式
-    #         _, jpeg = cv2.imencode('.jpg', frame_data)
-    #
-    #         # 生成MJPEG流格式数据
-    #         yield (b'--frame\r\n'
-    #                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+            # 生成MJPEG流格式数据
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
 @app.route("/")
 def index():
     # return the rendered template
