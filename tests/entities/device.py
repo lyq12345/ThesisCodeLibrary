326484f5c6f9b0ef07a97859cd4ae83a0848819e
lyq12345 <yuqiaol5@uci.edu>
2023-11-19 15:59:28 -0800

    xxx

---
+++ tests/entities/device.py
@@ -0,0 +1,12 @@
+class Device:
+    def __init__(self, id, model, location):
+        self.id = id
+        self.location = location
+
+    def deploy_operator(self, op_id):
+        pass
+    def set_profile(self, cpu, memory, storage, battery):
+        pass
+    def get_status(self):
+        pass
+
--- tests/power_test/draw_graph.py
+++ tests/power_test/draw_graph.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import os
 
 # 设置文件夹路径
@@ -9,18 +9,51 @@ folder_path = 'results'
 total_power = 0
 count = 0
 
-# 遍历文件夹中的所有文件
-for filename in os.listdir(folder_path):
-    if filename.endswith(".csv"):
-        file_path = os.path.join(folder_path, filename)
+def calculate_mean():
+    # 遍历文件夹中的所有文件
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(folder_path, filename)
 
-        # 读取CSV文件
-        df = pd.read_csv(file_path)
+            # 读取CSV文件
+            df = pd.read_csv(file_path)
 
-        # 检查是否有"power"列
-        if "power" in df.columns:
-            # 计算"power"列的平均值并累加到总和中
-            power_mean = df["power"].mean()
-            print(f"average power for {filename}: {power_mean}")
+            # 检查是否有"power"列
+            if "power" in df.columns:
+                # 计算"power"列的平均值并累加到总和中
+                power_mean = df["power"].mean()
+                print(f"average power for {filename}: {power_mean}")
+
+def draw_emp_dist(filename):
+    df = pd.read_csv(filename)
+    data_list = df['proc_time'].to_list()
+    sorted_list = sorted(data_list)
+
+    plt.figure(figsize=(6, 4))
+    last, i = min(sorted_list), 0
+    while i < len(sorted_list):
+        plt.plot([last, sorted_list[i]], [i / len(sorted_list), i / len(sorted_list)], 'k')
+        if i < len(sorted_list):
+            last = sorted_list[i]
+        i += 1
+    plt.show()
+
+def draw_hist():
+    devices = ['pi', 'nano', 'xaiver']
+    versions = ['tinyyolov3', 'yolov3']
+    fig, axes = plt.subplots(2, 3, sharex='col', sharey='row')
+    for i in range(2):
+        version_name = versions[i]
+        for j in range(3):
+            device_name = devices[j]
+            file_name = f"results/power_{device_name}_fire_{version_name}.csv"
+            df = pd.read_csv(file_name)
+            data = df['power'].to_list()
+            axes[i, j].hist(data, bins=10, edgecolor='k', color='orange')
+            axes[i, j].set_title(f'{version_name} on {device_name}')
+    plt.show()
+
+# draw_emp_dist("results/raw_nano_human_tinyyolov3.csv")
+draw_hist()
 
 
