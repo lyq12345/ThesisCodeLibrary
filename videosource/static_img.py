56e732d9432f921921d0d85b30690bd606aeb0ea
lyq12345 <yuqiaol5@uci.edu>
2024-01-25 20:16:11 -0800

    qos evaluation data

--- videosource/static_img.py
+++ videosource/static_img.py
@@ -5,12 +5,14 @@ import sys
 
 host_device = sys.argv[1]
 port_number = sys.argv[2]
+operator_type = sys.argv[3]
+interval = float(sys.argv[4])
+
 hosts = {
     'pi': '111',
     'nano': '128.200.218.98',
     'xavier': '128.200.218.123'
 }
-operator_type = sys.argv[3]
 
 host_ip = hosts[host_device]
 # human detection url
@@ -22,11 +24,10 @@ processing_endpoint = f'http://{host_ip}:{port_number}/process_video'
 # processing_endpoint = 'http://node6:8849/process_video'
 # processing_endpoint = 'http://localhost:8849/process_video'
 
-# 定义发送请求的间隔时间（秒）
-interval = 1
 # camera_index = 0
 # cap = cv2.VideoCapture(camera_index)
 # img_path = "test_imgs/fire.jpg"
+
 img_path = "test_imgs/human.jpeg" if operator_type=="human" else "test_imgs/fire.jpg"
 frame = cv2.imread(img_path)
 
@@ -47,26 +48,13 @@ _, jpeg_frame = cv2.imencode('.jpg', resized_img, encode_params)
 processing_endpoint = str(processing_endpoint)
 
 while True:
-    # ret, frame = cap.read()
-
-    # calculate fps
-    # current_time = time.time()
-    # elapsed_time = current_time - start_time
-    # fps = 1 / elapsed_time
-    # start_time = current_time
-    #
-    # fps_text = "FPS: {:.2f}".format(fps)
-
-    # print("1111")
-
-    # send the encoded frame to processors
     try:
         response = requests.post(processing_endpoint, data=jpeg_frame.tobytes(),
                                  headers={'Content-Type': 'image/jpeg'})
         if response.status_code == 200:
             result = response.json()
             proc_time = round(float(result['process_time']), 4)
-            print(f"request succeeded, time: {proc_time}")
+            print(f"{proc_time}")
         response.raise_for_status()
     except requests.exceptions.Timeout:
         print(f'Request to {processing_endpoint} timed out.')
@@ -77,5 +65,5 @@ while True:
         print(e)
 
     # 等待指定的时间间隔
-    # time.sleep(interval)
+    time.sleep(interval)
 # cap.release()
