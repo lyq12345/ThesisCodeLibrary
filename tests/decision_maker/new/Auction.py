9d2cef685aa19744bbef216e7be74ee88c671def
lyq12345 <yuqiaol5@uci.edu>
2024-02-21 03:03:25 -0800

    auction device fail

--- tests/decision_maker/new/Auction.py
+++ tests/decision_maker/new/Auction.py
@@ -1,6 +1,11 @@
 import copy
 import json
 import numpy as np
+import random
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from status_tracker.rescons_models import cpu_consumption
 # 从 JSON 文件中读取数据
 def read_json(filename):
     with open(filename, 'r') as json_file:
@@ -30,12 +35,44 @@ class Adaptation:
 
         self.transmission_matrix = np.load('../mock/transmission.npy')
         self.solution = read_json("../mock/solution.json")
+        self.consume_operators()
+        self.banned_devices = []
+
+    def deploy(self, mapping):
+        op_code = mapping[1]
+        dev_id = mapping[2]
+        dev_name = self.devices[dev_id]["model"]
+        op_load = mapping[3]
+        resource_requirements = self.operator_profiles[op_code]["requirements"]["system"]
+        resource_requirements["cpu"] = cpu_consumption(op_code, dev_name, op_load)
+
+        for type, amount in resource_requirements.items():
+            self.devices[dev_id]["resources"]["system"][type] -= amount
+
+    def consume_operators(self):
+        deployed_op_ids = []
+        for mapping in self.solution:
+            if mapping[0] in deployed_op_ids:
+                continue
+            self.deploy(mapping)
+            deployed_op_ids.append(mapping[0])
 
     def calculate_bid(self, mapping, microservice):
         pass
 
     def device_fail(self):
-        pass
+        active_devices = []
+        for mapping in self.solution:
+            if mapping[2] not in active_devices:
+                active_devices.append(mapping[2])
+        crushed_dev_id = random.choice(active_devices)
+        self.banned_devices.append(crushed_dev_id)
+        missed_ms_ids = []
+        for ms_id, mapping in enumerate(self.solution):
+            if mapping[2] == crushed_dev_id:
+                missed_ms_ids.append(ms_id)
+        return missed_ms_ids
+
     def device_disconnection(self):
         pass
     def emergent_request(self):
@@ -46,4 +83,3 @@ class Adaptation:
 
 if __name__ == '__main__':
     adaptation = Adaptation()
-    print("acc")
