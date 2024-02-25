8aa84637eb82ac7013fcb3e8eb4774402a12a773
lyq12345 <yuqiaol5@uci.edu>
2024-02-25 02:49:09 -0800

    model fit

--- tests/delay_test/results/analyze/fit_model.py
+++ tests/delay_test/results/analyze/fit_model.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.stats import pearsonr
 import numpy as np
+from scipy.optimize import curve_fit
 def draw_scatter_graph(x_values, y_values):
 
     # 绘制折线图
@@ -10,8 +11,9 @@ def draw_scatter_graph(x_values, y_values):
     # 添加标题和标签
     # plt.title('test')
     plt.xlabel('invocation frequency(times/s)')
-    plt.ylabel('memory_usage(MiB)')
-    plt.ylim(0, 1000)
+    # plt.ylabel('memory_usage(MiB)')
+    plt.ylabel('cpu_load(%)')
+    # plt.ylim(0, 1000)
 
     # 显示图例
     plt.legend()
@@ -52,7 +54,38 @@ x2 = data['memory']
 x4 = data['interval']
 x4_freq = [1/item for item in x4]
 print(x4_freq)
-draw_scatter_graph(x4_freq, x2)
+# draw_scatter_graph(x4_freq, x1)
 # calculate_p_value(x1, y)
 # calculate_p_value(x2, y)
 # calculate_standard_deviation(y)
+
+# def func(x, a, b, c):
+#     return a * np.exp(-b * x) + c
+def log_model(x, a, b):
+    return a * np.log(x) + b
+
+def power_model(x, a, b, c):
+    return a * np.power(x, b) + c
+
+popt, pcov = curve_fit(power_model, x4_freq, x1, maxfev=10000)
+
+a_fit, b_fit, c_fit = popt
+print(a_fit, b_fit, c_fit)
+
+# 评估拟合效果
+residuals = x1 - power_model(x4_freq, a_fit, b_fit, c_fit)
+ss_res = np.sum(residuals**2)
+ss_tot = np.sum((x1 - np.mean(x1))**2)
+r_squared = 1 - (ss_res / ss_tot)
+print("R-squared:", r_squared)
+
+print(power_model(1.2, a_fit, b_fit, c_fit))
+
+# 绘制原始数据和拟合曲线
+plt.scatter(x4_freq, x1, label='Original Data')
+plt.plot(x4_freq, power_model(x4_freq, a_fit, b_fit, c_fit), color='red', label='Fitted Curve')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.title('Power Model Fitting')
+plt.show()
--- tests/status_tracker/rescons_models.py
+++ tests/status_tracker/rescons_models.py
@@ -1,8 +1,226 @@
 import copy
 import os
 import json
+import numpy as np
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+parameters = [
+  {
+    "jetson-nano": {
+        "a": 1,
+        "b": 1
+    },
+    "raspberrypi-4b": {
+        "a": 1,
+        "b": 1
+    },
+    "jetson-xavier": {
+        "a": 1,
+        "b": 1
+    },
+  },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": -717208.6734812426,
+            "b": -9.42588764691802e-05,
+            "c": 717247.4006012462
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+    {
+        "jetson-nano": {
+            "a": 1,
+            "b": 1
+        },
+        "raspberrypi-4b": {
+            "a": 1,
+            "b": 1
+        },
+        "jetson-xavier": {
+            "a": 1,
+            "b": 1
+        },
+    },
+]
+
+def log_model(x, a, b):
+    return a * np.log(x) + b
 def read_json(filename):
     with open(filename, 'r') as json_file:
         data = json.load(json_file)
